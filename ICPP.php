d7d0761e89200b196389199ee1abb5085c71bddf
felipefschwade <felipefaeschwade@gmail.com>
2015-12-28 15:34:57 -0300

    Aula 5 - Decorator - Atividade 2 - Modificando mais impostos e utilizando o padrão Decorator para soma-los

--- ICPP.php
+++ ICPP.php
@@ -1,12 +1,15 @@
 <?php
 	class ICPP extends TemplateImposto{
+		function __construct(Imposto $outroImposto = null){
+					parent::__construct($outroImposto);
+				}
 		protected function deveUsarMaximaTaxacao(Orcamento $Orcamento){
 			return $Orcamento->getValor() > 500;
 		}
 		protected function maximaTaxacao(Orcamento $Orcamento){
-			return $Orcamento->getValor() * 0.07;
+			return $Orcamento->getValor() * 0.07 + $this->calculaOutroImposto($Orcamento);
 		}
 		protected function minimaTaxacao(Orcamento $Orcamento){
-			return $Orcamento->getValor() * 0.05;
+			return $Orcamento->getValor() * 0.05 + $this->calculaOutroImposto($Orcamento);
 		}
 	}
--- IKCV.php
+++ IKCV.php
@@ -1,13 +1,16 @@
 <?php
 	class IKCV extends TemplateImposto{
+		function __construct(Imposto $outroImposto = null){
+					parent::__construct($outroImposto);
+				}
 		protected function deveUsarMaximaTaxacao(Orcamento $Orcamento){
 			return $Orcamento->getValor() > 500 && $this->temItemMaisCaroQue100($Orcamento);
 		}
 		protected function maximaTaxacao(Orcamento $Orcamento){
-			return $Orcamento->getValor() * 0.10;
+			return $Orcamento->getValor() * 0.10 + $this->calculaOutroImposto($Orcamento);
 		}
 		protected function minimaTaxacao(Orcamento $Orcamento){
-			return $Orcamento->getValor() * 0.06;
+			return $Orcamento->getValor() * 0.06 + $this->calculaOutroImposto($Orcamento);
 		}
 		protected function temItemMaisCaroQue100(Orcamento $Orcamento){
 			foreach ($Orcamento->getItens() as $item) {
